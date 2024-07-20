49139f59143899ed91a6ef0092cc4781fe15c110
Hardik Khariwal <128465167+hardikkhariwal297312@users.noreply.github.com>
2024-07-20 11:51:00 +0530

    Update 19_majorityElement.cpp

--- 19_majorityElement.cpp
+++ 19_majorityElement.cpp
@@ -1,12 +1,25 @@
 class Solution {
 public:
-    int majorityElement(vector<int>& nums) {
-        map<int,int> mp;
-        int ans=0;
-        for(int i:nums){
-            mp[i]++;
-            if(mp[i]>nums.size()/2){ans= i;}
+    // int majorityElement(vector<int>& nums) {
+    //     map<int,int> mp;
+    //     int ans=0;
+    //     for(int i:nums){
+    //         mp[i]++;
+    //         if(mp[i]>nums.size()/2){ans= i;}
+    //     }
+    //     return ans;
+    // }
+
+
+    //using moore voting algorithm 
+    int majorityElement(vector<int>& a){
+        int major=a[0],votes=1;
+        for(int i=1;i<a.size();i++){
+            if(a[i]==major){votes++;}
+            else{votes--;}
+
+            if(votes==0){major=a[i];votes++;}
         }
-        return ans;
+        return major;
     }
 };
